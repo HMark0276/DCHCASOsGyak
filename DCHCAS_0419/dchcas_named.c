6a3b79d0cbd94be800d75d74c08894433e16cb2c
HMárk <79007491+HMark0276@users.noreply.github.com>
2021-04-23 13:06:10 +0200

    Update dchcas_named.c

--- DCHCAS_0419/dchcas_named.c
+++ DCHCAS_0419/dchcas_named.c
@@ -1,60 +1,53 @@
-#include <sys/types.h>
+##include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-int main(){
-	pid_t cpid;
-	int ret;
-	int fd;
-	char buffer[32];
-	char fajlnev[]="fajlnev";
-	ret=mkfifo(fajlnev,0666);
-	if(ret==-1)
-	{
-		perror("mkfifo()");
-		exit(-1);
-	}
-	cpid=fork();
-	if(cpid==-1)
-	{
-		perror("fork()");
-		exit(-1);
-	}
-	if(cpid==0)
-	{
-		printf("%d: gyerek\n",getpid());
-		fd=open(fajlnev,O_WRONLY);
-		if(fd==-1)
-		{
-			perror("open()");
-			exit(-1);
-		}
-		strcopy(buffer,"Hengsperger Mark, DCHCAS");
-		write(fd,buffer,strlen(buffer));
-		printf("%d: gyerek - iras utan\n",getpid());
-		close(fd);
-	}
-	else
-	{
-		printf("%d: szulo\n",getpid());
-		fd=open(fajlnev,O_WRONLY);
-		if(fd==-1)
-		{
-			perror("open()");
-			exit(-1);
-		}
-		printf("%d: szulo - olvasas elott\n",getpid());
-		ret=read(fd,&buffer,sizeof(buffer));
-		printf("read() %d byte-ot olvasott, string: %s\n",ret,buffer);
-		close(fd);
-		unlink(fajlnev);
-	}
+#include <fcntl.h>  
+#include <stdio.h>                      
+#include <stdlib.h>                  
+#include <unistd.h>    
+#include <string.h>   
+int main(){      
+	pid_t cpid;    
+	int ret;       
+	int fd;     
+	char buffer[40];
+	char dchcas[]="dchcas";
+	ret=mkfifo(dchcas,0666); 
+	cpid=fork();         
+	if(ret==-1)      
+	{       
+		perror("mkfifo()");    
+		exit(-1);              
+	}                     
+	if(cpid==0)       
+	{      
+		printf("Gyerekprocessz PID...%d\n",getpid());
+		fd=open(dchcas,O_WRONLY);               
+		if(fd==-1)                   
+		{               
+			perror("open()");    
+			exit(-1);             
+		}                         
+		strcpy(buffer,"Hengsperger Mark, DCHCAS");     
+		write(fd,buffer,strlen(buffer)); 
+		printf("Gyerek PID iras utan...%d\n",getpid());      
+		close(fd);       
+	}           
+	else          
+	{              
+		printf("Szuloprocessz PID...%d\n",getpid());    
+		fd=open(dchcas,O_RDONLY);   
+		if(fd==-1)          
+		{                 
+			perror("open()");   
+			exit(-1);          
+		}                           
+		printf("Szulo PID olvasas elott...%d\n",getpid());    
+		ret=read(fd,&buffer,sizeof(buffer));                         
+		printf("A read() altal olvasott byte-ok...%d, a string...%s\n",ret,buffer);  
+		close(fd);                                                                     
+		unlink(dchcas);                                                                  
+	}                                                                                        
 	return 0;
-}
 
 		
